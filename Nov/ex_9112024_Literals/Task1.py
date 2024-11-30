dd1d462d25abe9c50c181b5301b114849506ff7e
himanshu <hbatra2609@gmail.com>
2024-12-01 04:05:26 +0530

    7th nov commit

---
+++ Nov/ex_9112024_Literals/Task1.py
@@ -0,0 +1,13 @@
+# enter 3 nos and print add, sub, mul, divide
+
+n1=int(input("Enter a number: "))
+n2=int(input("Enter a number: "))
+n3=int(input("Enter a number: "))
+sum=n1+n2+n3
+sub=n1-n2-n3
+mul=n1*n2*n3
+div=n1/n2/n3
+print(sum)
+print(sub)
+print(mul)
+print(div)
---
+++ Nov/ex_9112024_Literals/Task2.py
@@ -0,0 +1,7 @@
+# enter 2 no and find question and remainder
+n1= int(input("Enter a number: "))
+n2= int(input("Enter a number: ") )
+quo= n1/n2
+rem=n1%n2
+print(quo)
+print(rem)
