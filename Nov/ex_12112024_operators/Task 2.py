25733742de3885868e685ffe8c875b79d7088b76
himanshu <hbatra2609@gmail.com>
2024-12-01 17:15:26 +0530

    8th nov commit

---
+++ Nov/ex_12112024_operators/Task 2.py
@@ -0,0 +1,10 @@
+# prg 3: max no between 2 nos
+
+no1= int(input("Enter no1: "))
+no2= int(input("Enter no2: "))
+if(no1>no2):
+    max=no1
+    print("Max no is" + ' ' + str(max))
+else:
+    max=no2
+    print("Max no is" + ' '+ str(max))
---
+++ Nov/ex_12112024_operators/Task1.py
@@ -0,0 +1,12 @@
+# prg 1: age=21 can i go to club
+# prg 3: max no between 2 nos
+
+age = int(input("Enter the age"))
+if(age==21):
+    {
+        print("I can go to club")
+    }
+
+else:
+    print("I cannot go to club")
+
