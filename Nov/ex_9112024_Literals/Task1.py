a444d0e52b33e7b80845434ff1b4d8baa39e8bd4
himanshu <hbatra2609@gmail.com>
2024-12-01 16:54:20 +0530

    7th nov commit

--- Nov/ex_9112024_Literals/Task1.py
+++ Nov/ex_9112024_Literals/Task1.py
@@ -1,12 +1,12 @@
 # enter 3 nos and print add, sub, mul, divide
 
-n1=int(input("Enter a number: "))
-n2=int(input("Enter a number: "))
-n3=int(input("Enter a number: "))
+n1=float(input("Enter a number: "))
+n2=float(input("Enter a number: "))
+n3=float(input("Enter a number: "))
 sum=n1+n2+n3
 sub=n1-n2-n3
 mul=n1*n2*n3
-div=n1/n2/n3
+div=(n1/n2)/n3
 print(sum)
 print(sub)
 print(mul)
