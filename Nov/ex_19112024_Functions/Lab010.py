6560ffedaccb57f82529a23f207a0f9e263e4dfa
himanshu <hbatra2609@gmail.com>
2024-12-02 17:27:10 +0530

    9th nov commit

---
+++ Nov/ex_19112024_Functions/Lab010.py
@@ -0,0 +1,13 @@
+# print sum of 3 nos, if no input
+no1=int(input("Enter a no 1"))
+no2=int(input("Enter a no 2"))
+no3=int(input("Enter a no 3"))
+
+
+def sum(a=100,b=200,c=300):
+    return a+b+c
+
+result=sum(no1,no2,no3)
+print(result)
+dresult=sum()
+print(dresult)
---
+++ Nov/ex_19112024_Functions/Lab011.py
@@ -0,0 +1,10 @@
+# create pizza toppings for pizza lovers
+
+def pizza_lovers(*toppins):
+    print(toppins)
+    for toppin in toppins:
+        print(toppin)
+
+himanshu=pizza_lovers("onion", "capsicum")
+aarti=pizza_lovers("cheese"," jalapeno")
+
